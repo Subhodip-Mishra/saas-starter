3cf4cdf207cac319197ffb99670222afd0538e01
Suvodeep Mishra <subhodipmishra47@gmail.com>
2024-11-02 21:11:12 +0530

    asdf

--- middleware.ts
+++ middleware.ts
@@ -1,49 +1,10 @@
-import { clerkClient, clerkMiddleware, Client, createRouteMatcher } from '@clerk/nextjs/server'
-import { NextResponse } from 'next/server';
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-const publicRoutes = createRouteMatcher(["/", "/api/webhook/register", '/sign-up', '/sign-in'])
-
-export default clerkMiddleware({
-  publicRoutes,
-  async afterAuth(auth, req){
-    if(!auth.userId && !publicRoutes.includes(req.nextUrl.pathname)){
-      return NextResponse.redirect(new URL("/sign-in", req.url));
-    }
-
-    if(auth.userId){
-      try {
-        const client = await clerkClient()
-        const user = await client.users?.getUser(auth.userId)
-        const role = user.publicMetadata.role as string | undefined;
-
-        //admin role redirection
-        if(role === "admin" && req.nextUrl.pathname === "/dashboard"){
-          return NextResponse.redirect(new URL("/admin/dashboard", req.url));
-        }
-
-        //prevent non admin user to access the admin routes
-        if(role !== "admin" && req.nextUrl.pathname.startsWith("/admin")){
-          return NextResponse.redirect(new URL("/dashboard", req.url));
-        }
-
-        //redirect auth user tyring to access public routes
-        if(publicRoutes.includes(req.nextUrl.pathname)){
-          return NextResponse.redirect(
-            role === "admin" ? "/admin/dashboard" : "/dashboard",
-            req.url
-          );
-        }
-
-      } catch (error) {
-        console.error(error)
-        return NextResponse.redirect(new URL("/error", req.url));
-      }
-    }
-  }
-})
+export default clerkMiddleware()
 
 export const config = {
   matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
