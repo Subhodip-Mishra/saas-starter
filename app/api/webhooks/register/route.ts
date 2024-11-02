df33a4c6ee380a3fd8dce34fdf01249d42c548af
Suvodeep Mishra <subhodipmishra47@gmail.com>
2024-11-02 21:20:24 +0530

    asd

--- app/api/webhooks/register/route.ts
+++ app/api/webhooks/register/route.ts
@@ -1,28 +1,29 @@
-import { Webhook } from "svix";
-import { headers } from "next/headers";
-import { WebhookEvent } from "@clerk/nextjs/server";
-import prisma from "@/lib/prisma";
+import { Webhook } from "svix"
+import { headers } from "next/headers"
+import { WebhookEvent } from "@clerk/nextjs/server"
+import prisma from "@/lib/prisma"
+
 
 export async function POST(req: Request) {
-    const webhook_secret = process.env.WEBHOOK_SECRET;
+    const webhook_secrect = process.env.WEBHOOK_SECRECT;
 
-    if (!webhook_secret) {
-        throw new Error("Please add WEBHOOK_SECRET in environment variables.");
+    if (!webhook_secrect) {
+        throw new Error("please add webhook in env")
     }
 
-    const headerPayload = headers();
-    const svix_id = headerPayload.get("svix-id");
-    const svix_timestamp = headerPayload.get("svix-timestamp");
-    const svix_signature = headerPayload.get("svix-signature");
+    const headerPayload = await headers();
+    const svix_id = headerPayload.get("svix-id")
+    const svix_timestamp = headerPayload.get("svix-timestamp")
+    const svix_signature = headerPayload.get("svix-signature")
 
     if (!svix_id || !svix_timestamp || !svix_signature) {
-        return new Response("Error occurred - No Svix header", { status: 400 });
+        return new Response("Error occurred - No Svix header");
     }
 
     const payload = await req.json();
     const body = JSON.stringify(payload);
 
-    const wh = new Webhook(webhook_secret);
+    const wh = new Webhook(webhook_secrect);
 
     let event: WebhookEvent;
 
@@ -30,43 +31,51 @@ export async function POST(req: Request) {
         event = wh.verify(body, {
             "svix-id": svix_id,
             "svix-timestamp": svix_timestamp,
-            "svix-signature": svix_signature,
+            "svix-signature": svix_signature
         }) as WebhookEvent;
     } catch (error) {
         console.error("Error verifying webhook", error);
-        return new Response("Error verifying webhook", { status: 400 });
+        return new Response("Error occurred", { status: 400 });
     }
 
-    const eventType = event.type;
+    const eventType = event.type
+
+    // logs
 
     if (eventType === "user.created") {
         try {
-            const { email_addresses, primary_email_address_id } = event.data;
-            console.log("Email addresses:", email_addresses, "Primary Email ID:", primary_email_address_id);
+            const { email_addresses, primary_email_address_id } = event.data
+            console.log("email addresses", email_addresses, primary_email_address_id)
 
+
+            //optional
             const primaryEmail = email_addresses.find(
                 (email) => email.id === primary_email_address_id
-            );
+            )
 
             if (!primaryEmail) {
-                return new Response("No primary email found", { status: 400 });
+                return new Response("NO Primary email found", { status: 400 });
             }
+
+            //create a user in neon(postgresql)
 
             const newUser = await prisma.user.create({
                 data: {
-                    id: event.data.id,
+                    id: event.data.id!,
                     email: primaryEmail.email_address,
-                    isSubscribed: false,
-                },
-            });
+                    isSubscribed: false
+                }
+            })
+            console.log("New user created", newUser);
 
-            console.log("New user created:", newUser);
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                return new Response("Error creating user in database", { status: 400 });
+            }
 
-        } catch (error) {
-            console.error("Error creating user in database", error);
-            return new Response("Error creating user in database", { status: 500 });
         }
     }
 
-    return new Response("Webhook received successfully", { status: 200 });
+    return new Response("Webhook recived successfully", { status: 200 });
+
 }
